c30fbe6a01af435cedf9f51ea927362f0a706a11
MiltonIT19A <70194185+MiltonIT19A@users.noreply.github.com>
2020-09-08 09:40:10 +0200

    alshibab

---
+++ Programmering 1/Variabler_IO/index.js
@@ -0,0 +1,13 @@
+name = "Milton" 
+
+console.log(`Hej ${name}`)
+
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+medelvärde = (tal1+tal2+tal3)/3
+
+console.log(`${tal1}+${tal2}+${tal3})/3 = ${medelvarde}`)
+
+let langd = prompt ("ange värdet för rektangelns längd ")
