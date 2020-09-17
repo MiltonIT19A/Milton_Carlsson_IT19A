1d09abeacd8a69fe1dbf235d4bb02b8dfb1f0aa0
MiltonIT19A <70194185+MiltonIT19A@users.noreply.github.com>
2020-09-17 09:51:57 +0200

    Mycket

---
+++ Programmering 1/if_satser/if_sats2.js
@@ -0,0 +1,33 @@
+
+
+/* let age = prompt (`Hur gammal är du?`)
+console.log (`Ålder: ${age}`)
+
+if (age >= 18){
+    console.log("Du får se en film upp till 18 års gräns")
+} else if (age >= 15){
+    console.log("Du får se en film upp till 15 års gräns")
+} else if (age >= 7){
+    console.log("Du får se en film upp till 7 års gräns")
+} else {
+    console.log("Du får inte se på filmen")
+}
+*/ 
+
+let bla = prompt (`Hur många gånger om dagen åker du spårvagn`)
+
+if (bla >= 10){
+    console.log("Det är värt att köpa månadskort")
+} else if (bla >= 6){
+    console.log("Det kan vara värt att köpa månadskort")
+} else if (bla >= 2){
+    console.log("Det är inte värt att köpa månadskort")
+} else {
+    console.log("Det är inte värt att köpa månadskort")
+}
+
+
+
+
+
+
