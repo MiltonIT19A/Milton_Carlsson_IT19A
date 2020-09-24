8bb9e10bb631a8f5e4c49d09fa21799d80b0f0a4
MiltonIT19A <70194185+MiltonIT19A@users.noreply.github.com>
2020-09-24 09:29:45 +0200

    kokokokoko

---
+++ Programmering 1/For_sats/for1.js
@@ -0,0 +1,15 @@
+let p_talserie1 = document.querySelector("#talserie1")
+let p_talserie2 = document.querySelector("#talserie2")
+
+
+for (let i=1; i<=10; i++){
+p_talserie1.innerHTML += `${i} `
+}
+
+for (let j=10; j>0; j--)
+p_talserie2.innerHTML += `${j} `
+
+
+for (let i = 0; i<10; i++){
+console.log(`${i} I will not write all over the walls`)
+}
--- Programmering 1/Hemupgift/if_sats3.js
+++ Programmering 1/Hemupgift/if_sats3.js
@@ -1,7 +1,13 @@
 let vikt = prompt ("Hur tungt är ditt bagage (kg)")
+
 let längd = prompt ("Hur långt är ditt bagage (cm)")
+
 let bredd = prompt ("hur brett är ditt bagage (cm)")
+
 let höjd = prompt ("hur högt är ditt bagage (cm)")
+
+
+
 
 if (vikt <= 8 && längd  <= 55 && bredd <= 40 && höjd <= 23){
     console.log("Ditt bagage får åka med")
