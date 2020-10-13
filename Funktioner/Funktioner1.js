9786c2a064e7bb586baefba9cc6ed3f080b04c5c
MiltonIT19A <70194185+MiltonIT19A@users.noreply.github.com>
2020-10-13 09:38:49 +0200

    asd

---
+++ Funktioner/Funktioner1.js
@@ -0,0 +1,44 @@
+let namn = "Milton" // den här variabeln har ett globalt scope
+
+function sayHello(){
+    let age = 17 // lokal variabel, synlig i funktionen och inte utanför
+    console.log(`Hej ${namn}`)
+}
+
+// Anropar funktioner sayHello
+sayHello()
+sayHello()
+
+
+function addera(tal1, tal2){
+    let summa = tal1+tal2
+    return summa
+}
+
+console.log(`2+5=${addera(2,5)}`)
+
+
+function subtrahera(tal1, tal2){
+    let summa = tal1-tal2
+    return summa
+}
+
+console.log(`8-7=${subtrahera(8,7)}`)
+
+
+function dividera(tal1, tal2){
+    if (tal2 == 0){
+        return "Du ej dela med 0"
+    }
+    let kvot = tal1/tal2
+    return kvot
+}
+
+console.log(`8/2=${dividera(8,2)}`)
+
+function multiplicera(tal1, tal2){
+    let summa = tal1*tal2
+    return summa
+}
+
+console.log(`5*2=${multiplicera(5,2)}`)
